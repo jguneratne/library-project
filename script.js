cb6ef6ad7668e94f4d602775135d3bd3078561ae
jguneratne <59374962+jguneratne@users.noreply.github.com>
2023-03-10 14:23:37 -0500

    Begin brainstorming script

---
+++ script.js
@@ -0,0 +1,14 @@
+let myLibrary = []; // Store book info from addBookToLibrary user input
+
+function Book(title, author, pages, status) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.status = status;
+}
+
+function addBookToLibrary() {
+  // Take user input and feed it into new Book instantiation
+  // Feed input into myLibrary array
+  // Use info to create new Library card in DOM
+}
