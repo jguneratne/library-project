c9e43e9cd1606ff88a9b37b757ec973ab9071411
jguneratne <59374962+jguneratne@users.noreply.github.com>
2023-03-13 07:22:34 -0400

    Add for loop to iterate over array

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-let myLibrary = []; // Store book info from addBookToLibrary user input
+let myLibrary = [
+  // Store book info from addBookToLibrary user input
+  ["The Hobbit", "JRR Tolkein", "295", "Read"],
+];
 
 function Book(title, author, pages, status) {
   this.title = title;
@@ -10,13 +13,22 @@ function Book(title, author, pages, status) {
 function addBookToLibrary() {
   // Take user input and feed it into new Book instantiation
   // Feed input into myLibrary array
+
+  const newCard = document.querySelector(".add");
 }
 
 function displayBooks() {
   // Loop through array to display each book (in a Table or in their own cards)
-}
+  for (let i = 0; i < myLibrary.length; i++) {
+    // get the size of the inner array
+    var innerArrayLength = myLibrary[i].length;
 
-//Button - how to make form pop up on button click?
+    // loop the inner array
+    for (let j = 0; j < innerArrayLength; j++) {
+      console.log("[" + i + "," + j + "] = " + myLibrary[i][j]);
+    }
+  }
+}
 
 function showForm() {
   const form = document.querySelector(".form-div");
@@ -24,9 +36,11 @@ function showForm() {
   form.style.display = "block";
 }
 
-function clicked() {
-  console.log("Click");
-}
+// function clicked() {
+//   console.log("Click");
+// }
 
 const addBookBtn = document.querySelector(".add-book");
 addBookBtn.addEventListener("pointerdown", showForm);
+
+displayBooks();
