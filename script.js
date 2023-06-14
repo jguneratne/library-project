6ac95010dd980d3ef5f3872198837b6f908e716c
jguneratne <59374962+jguneratne@users.noreply.github.com>
2023-06-14 18:35:45 -0400

    Move replaceCards and createCard methods to Book class and call on submit - not working yet

--- script.js
+++ script.js
@@ -19,6 +19,148 @@ class Book {
     document.forms[0].reset(); // Clears form for next entry
   }
 
+  createCard() {
+    for (let i = 0; i < myLibrary.length; i++) {
+      const libEntry = document.querySelector(".bookshelf");
+
+      const newCard = document.createElement("div");
+      newCard.className = "new-card";
+      newCard.id = "new-card" + "-" + i;
+      newCard.setAttribute("data-card-num", i);
+
+      const cardInfo = document.createElement("div");
+      cardInfo.className = "card-info";
+
+      const title = document.createElement("h4");
+      title.className = "title";
+      title.textContent = "Title:";
+
+      const titleValue = document.createElement("p");
+      titleValue.className = "title-value";
+      titleValue.textContent = `${myLibrary[i].title}`;
+
+      const author = document.createElement("h4");
+      author.className = "author";
+      author.textContent = "Author:";
+
+      const authorValue = document.createElement("p");
+      authorValue.className = "author-value";
+      authorValue.textContent = `${myLibrary[i].author}`;
+
+      const pages = document.createElement("h4");
+      pages.className = "pages";
+      pages.textContent = "Pages:";
+
+      const pagesValue = document.createElement("p");
+      pagesValue.className = "pages-value";
+      pagesValue.textContent = `${myLibrary[i].pages}`;
+
+      const status = document.createElement("h4");
+      status.className = "status";
+      status.textContent = "Finished:";
+
+      const statusValue = document.createElement("INPUT");
+      statusValue.setAttribute("type", "checkbox");
+      statusValue.className = "statusValue";
+      statusValue.name = "cardInfo";
+      statusValue.id = "show-status";
+      statusValue.value = "read";
+      statusValue.checked = myLibrary[i].status;
+
+      const removeBtn = document.createElement("button");
+      removeBtn.type = "button";
+      removeBtn.value = "remove";
+      removeBtn.className = "remove";
+      removeBtn.textContent = "Remove Book";
+
+      libEntry.appendChild(newCard);
+      newCard.appendChild(cardInfo);
+      cardInfo.appendChild(title);
+      cardInfo.appendChild(titleValue);
+      cardInfo.appendChild(author);
+      cardInfo.appendChild(authorValue);
+      cardInfo.appendChild(pages);
+      cardInfo.appendChild(pagesValue);
+      cardInfo.appendChild(status);
+      cardInfo.appendChild(statusValue);
+      newCard.appendChild(removeBtn);
+
+      statusValue.addEventListener(
+        "change",
+        (Book.prototype.toggleReadStatus = function () {
+          if (myLibrary[i].status === true) {
+            myLibrary[i].status = false;
+          } else {
+            myLibrary[i].status === false;
+            myLibrary[i].status = true;
+          }
+        })
+      );
+
+      //Mouse events to remove books
+
+      removeBtn.addEventListener(
+        "pointerdown",
+        (Book.prototype.removeArrayElement = function () {
+          let arrayPosition = newCard.dataset.cardNum;
+
+          console.log(arrayPosition);
+
+          let removedElement = myLibrary.splice(arrayPosition, 1);
+          console.log(removedElement);
+        })
+      );
+
+      removeBtn.addEventListener(
+        "pointerdown",
+        (Book.prototype.removeCard = function () {
+          const parent = document.querySelector(".bookshelf");
+          let deleteCard = document.querySelector("[data-card-num]");
+
+          let removedCard = parent.removeChild(deleteCard);
+          console.log(removedCard);
+
+          Book.prototype.replaceCards();
+          Book.prototype.createCard();
+        })
+      );
+
+      //Keybarod events to remove books
+
+      removeBtn.addEventListener("keydown", function (e) {
+        if (e.key === "Enter") {
+          arrayPosition = newCard.dataset.cardNum;
+
+          console.log(arrayPosition);
+
+          removedElement = myLibrary.splice(arrayPosition, 1);
+          console.log(removedElement);
+        }
+      });
+
+      removeBtn.addEventListener("keydown", function (e) {
+        if (e.key === "Enter") {
+          parent = document.querySelector(".bookshelf");
+          deleteCard = document.querySelector("[data-card-num]");
+
+          removedCard = parent.removeChild(deleteCard);
+          console.log(removedCard);
+
+          Book.prototype.replaceCards();
+          Book.prototype.createCard();
+        }
+      });
+    }
+  }
+
+  replaceCards() {
+    // Replaces all cards on each new card creation to prevent duplicates
+    let parent = document.querySelector(".bookshelf");
+    while (parent.firstChild) {
+      parent.removeChild(parent.firstChild);
+    }
+  }
+
   getBookData(e) {
     e.preventDefault();
 
@@ -33,170 +175,6 @@ class Book {
 
 const bookEntry = new Book(this.title, this.author, this.pages, this.read);
 
-//
-//
-//
-//
-//  OLD CODE:
-
-// Book.prototype.getBookData = function (e) {
-//   e.preventDefault(); // Stop form from submitting to server
-
-//   const bookEntry = new Book(
-//     document.querySelector("#title").value,
-//     document.querySelector("#author").value,
-//     document.querySelector("#pages").value,
-//     document.querySelector("#status").checked
-//   );
-
-//   myLibrary.push(bookEntry);
-
-//   Book.prototype.replaceCards();
-//   Book.prototype.createCard();
-// };
-
-Book.prototype.createCard = function () {
-  for (let i = 0; i < myLibrary.length; i++) {
-    const libEntry = document.querySelector(".bookshelf");
-
-    const newCard = document.createElement("div");
-    newCard.className = "new-card";
-    newCard.id = "new-card" + "-" + i;
-    newCard.setAttribute("data-card-num", i);
-
-    const cardInfo = document.createElement("div");
-    cardInfo.className = "card-info";
-
-    const title = document.createElement("h4");
-    title.className = "title";
-    title.textContent = "Title:";
-
-    const titleValue = document.createElement("p");
-    titleValue.className = "title-value";
-    titleValue.textContent = `${myLibrary[i].title}`;
-
-    const author = document.createElement("h4");
-    author.className = "author";
-    author.textContent = "Author:";
-
-    const authorValue = document.createElement("p");
-    authorValue.className = "author-value";
-    authorValue.textContent = `${myLibrary[i].author}`;
-
-    const pages = document.createElement("h4");
-    pages.className = "pages";
-    pages.textContent = "Pages:";
-
-    const pagesValue = document.createElement("p");
-    pagesValue.className = "pages-value";
-    pagesValue.textContent = `${myLibrary[i].pages}`;
-
-    const status = document.createElement("h4");
-    status.className = "status";
-    status.textContent = "Finished:";
-
-    const statusValue = document.createElement("INPUT");
-    statusValue.setAttribute("type", "checkbox");
-    statusValue.className = "statusValue";
-    statusValue.name = "cardInfo";
-    statusValue.id = "show-status";
-    statusValue.value = "read";
-    statusValue.checked = myLibrary[i].status;
-
-    const removeBtn = document.createElement("button");
-    removeBtn.type = "button";
-    removeBtn.value = "remove";
-    removeBtn.className = "remove";
-    removeBtn.textContent = "Remove Book";
-
-    libEntry.appendChild(newCard);
-    newCard.appendChild(cardInfo);
-    cardInfo.appendChild(title);
-    cardInfo.appendChild(titleValue);
-    cardInfo.appendChild(author);
-    cardInfo.appendChild(authorValue);
-    cardInfo.appendChild(pages);
-    cardInfo.appendChild(pagesValue);
-    cardInfo.appendChild(status);
-    cardInfo.appendChild(statusValue);
-    newCard.appendChild(removeBtn);
-
-    statusValue.addEventListener(
-      "change",
-      (Book.prototype.toggleReadStatus = function () {
-        if (myLibrary[i].status === true) {
-          myLibrary[i].status = false;
-        } else {
-          myLibrary[i].status === false;
-          myLibrary[i].status = true;
-        }
-      })
-    );
-
-    //Mouse events to remove books
-
-    removeBtn.addEventListener(
-      "pointerdown",
-      (Book.prototype.removeArrayElement = function () {
-        let arrayPosition = newCard.dataset.cardNum;
-
-        console.log(arrayPosition);
-
-        let removedElement = myLibrary.splice(arrayPosition, 1);
-        console.log(removedElement);
-      })
-    );
-
-    removeBtn.addEventListener(
-      "pointerdown",
-      (Book.prototype.removeCard = function () {
-        const parent = document.querySelector(".bookshelf");
-        let deleteCard = document.querySelector("[data-card-num]");
-
-        let removedCard = parent.removeChild(deleteCard);
-        console.log(removedCard);
-
-        Book.prototype.replaceCards();
-        Book.prototype.createCard();
-      })
-    );
-
-    //Keybarod events to remove books
-
-    removeBtn.addEventListener("keydown", function (e) {
-      if (e.key === "Enter") {
-        arrayPosition = newCard.dataset.cardNum;
-
-        console.log(arrayPosition);
-
-        removedElement = myLibrary.splice(arrayPosition, 1);
-        console.log(removedElement);
-      }
-    });
-
-    removeBtn.addEventListener("keydown", function (e) {
-      if (e.key === "Enter") {
-        parent = document.querySelector(".bookshelf");
-        deleteCard = document.querySelector("[data-card-num]");
-
-        removedCard = parent.removeChild(deleteCard);
-        console.log(removedCard);
-
-        Book.prototype.replaceCards();
-        Book.prototype.createCard();
-      }
-    });
-  }
-};
-
-Book.prototype.replaceCards = function () {
-  // Replaces all cards on each new card creation to prevent duplicates
-  let parent = document.querySelector(".bookshelf");
-  while (parent.firstChild) {
-    parent.removeChild(parent.firstChild);
-  }
-};
-
 // Global Variables
 const bookFormBtn = document.querySelector(".show-form-btn");
 const formDiv = document.querySelector(".form-div");
@@ -209,6 +187,8 @@ bookFormBtn.addEventListener("pointerdown", bookEntry.showForm);
 
 form.addEventListener("submit", bookEntry.getBookData);
 form.addEventListener("submit", bookEntry.hideForm);
+form.addEventListener("submit", bookEntry.replaceCards);
+form.addEventListener("submit", bookEntry.createCard);
 
 bookFormBtn.addEventListener("keydown", function (e) {
   if (e.key === "Enter") {
